fc84845b95ef547fc0b4a74c0bb58ce12a7ad03c
Kirill <89972203+kirillklimenko@users.noreply.github.com>
2023-06-27 09:58:55 +0300

    Add json.loads

--- async_request.py
+++ async_request.py
@@ -1,5 +1,6 @@
 import aiohttp
 import asyncio
+import json
 import logging
 import pandas as pd
 
@@ -32,13 +33,11 @@ async def async_requests(dataframe, extractor=None):
         logging.info({"id": index} | row.to_dict())
         async with caller[row.method.upper()](row.url, **options) as resp:
             length = 0 if pd.isnull(resp.content_length) else resp.content_length
-            content = await resp.read()
-            response = extractor(content) if extractor is not None else content
             dataframe.at[index, "status"] = resp.status
             dataframe.at[index, "length"] = length
             dataframe.at[index, "target"] = resp.url
             dataframe.at[index, "headers"] = dict(resp.headers)
-            dataframe.at[index, "response"] = response
+            dataframe.at[index, "response"] = await resp.read()
 
     timeout = aiohttp.ClientTimeout(total=None)
     async with aiohttp.ClientSession(timeout=timeout) as session:
@@ -47,6 +46,10 @@ async def async_requests(dataframe, extractor=None):
             for index, row  in dataframe.iterrows()
         )
         await asyncio.gather(*tasks)
+
+    if extractor is not None:
+        dataframe.response = dataframe.response.apply(extractor)
+
     return dataframe.astype({"status": "Int16", "length": "Int64"})
 
 
@@ -57,13 +60,14 @@ def main():
         "LOC_CODE": "string",
         "SITE_LOCATIONS": "string"
     })
+    df.columns = df.columns.str.lower()
     params = df.to_dict("records")
     df["url"] = "https://id.execute-api.region.amazonaws.com/stage/commands"
     df["method"] = "GET"
     df["params"] = params
     df["headers"] = [{"Content-Type": "application/json"}] * len(df)
 
-    result = asyncio.run(async_requests(df))
+    result = asyncio.run(async_requests(df, extractor=json.loads))
     print(result[["url", "status", "response"]])
 
 
