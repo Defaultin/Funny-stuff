c9a2ef50128c2ddc64a6948097ea41de48984ff6
Kirill <89972203+kirillklimenko@users.noreply.github.com>
2024-12-22 16:52:09 +0200

    Add files via upload

---
+++ buy_sell_multiple_stocks.py
@@ -0,0 +1,39 @@
+"""
+You are given an integer array prices where prices[i]
+is the price of a given stock on the ith day.
+On each day, you may decide to buy and/or sell the stock.
+You can only hold at most one share of the stock at any time.
+However, you can buy it then immediately sell it on the same day.
+Find and return the maximum profit you can achieve.
+
+Examples:
+Input: prices = [7,1,5,3,6,4]
+Output: 7
+Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+Total profit is 4 + 3 = 7.
+
+Input: prices = [1,2,3,4,5]
+Output: 4
+Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
+Total profit is 4.
+
+Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transactions are done and the max profit = 0.
+"""
+
+
+def buy_sell_multiple_stocks(prices: list[int]) -> int:
+    profit = 0
+    for i in range(1, len(prices)):
+        if prices[i] > prices[i - 1]:
+            profit += prices[i] - prices[i - 1]
+
+    return profit
+
+
+if __name__ == "__main__":
+    given = [7, 1, 5, 3, 6, 4]
+    result = buy_sell_multiple_stocks(given)
+    print(result)
