e1d527739fdd861e871fd91ad4313bc6ab4c4b40
Kirill <89972203+kirillklimenko@users.noreply.github.com>
2024-12-25 13:55:16 +0200

    Update two_sum.py

--- two_sum.py
+++ two_sum.py
@@ -6,30 +6,47 @@ and you may not use the same element twice.
 You can return the answer in any order.
 
 Examples:
-Input: nums = [2,7,11,15], target = 9
+Input: numbers = [2,7,11,15], target = 9
 Output: [0,1]
-Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Explanation: Because numbers[0] + numbers[1] == 9, we return [0, 1].
 
-Input: nums = [3,2,4], target = 6
+Input: numbers = [3,2,4], target = 6
 Output: [1,2]
 Example 3:
 
-Input: nums = [3,3], target = 6
+Input: numbers = [3,3], target = 6
 Output: [0,1]
 """
 
 
-def two_sum(nums: list[int], target: int) -> list[int]:
+def two_sum(numbers: list[int], target: int) -> list[int]:
     indexes = {}
 
-    for i, num in enumerate(nums):
+    for i, num in enumerate(numbers):
         if target - num in indexes:
             return [indexes[target - num], i]
 
         indexes[num] = i
 
 
+def two_sum_sorted(numbers: list[int], target: int) -> list[int]:
+    left, right = 0, len(numbers) - 1
+
+    while left < right:
+        total = numbers[left] + numbers[right]
+
+        if total == target:
+            return [left, right]
+        elif total < target:
+            left += 1
+        else:
+            right -= 1
+
+
 if __name__ == "__main__":
     given = [2, 7, 11, 15]
     result = two_sum(given, 9)
+    print(result)
+
+    result = two_sum_sorted(given, 9)
     print(result)
