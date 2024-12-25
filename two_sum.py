fffc06161568e108fe614db1701d8c3b664b52d7
Kirill <89972203+kirillklimenko@users.noreply.github.com>
2024-12-25 14:46:45 +0200

    Update two_sum.py

--- two_sum.py
+++ two_sum.py
@@ -43,10 +43,46 @@ def two_sum_sorted(numbers: list[int], target: int) -> list[int]:
             right -= 1
 
 
+def three_sum(numbers: list[int], target: int) -> list[list[int]]:
+    numbers.sort()
+    result = []
+
+    for i in range(len(numbers) - 2):
+        if i > 0 and numbers[i] == numbers[i - 1]:
+            continue
+
+        left, right = i + 1, len(numbers) - 1
+
+        while left < right:
+            total = numbers[i] + numbers[left] + numbers[right]
+
+            if total == target:
+                result.append([numbers[i], numbers[left], numbers[right]])
+
+                while left < right and numbers[left] == numbers[left + 1]:
+                    left += 1
+
+                while left < right and numbers[right] == numbers[right - 1]:
+                    right -= 1
+
+                left += 1
+                right -= 1
+            elif total < target:
+                left += 1
+            else:
+                right -= 1
+
+    return result
+
+
 if __name__ == "__main__":
     given = [2, 7, 11, 15]
     result = two_sum(given, 9)
     print(result)
 
     result = two_sum_sorted(given, 9)
+    print(result)
+
+    given = [-1, 0, 1, 2, -1, -4]
+    result = three_sum(given, 0)
     print(result)
