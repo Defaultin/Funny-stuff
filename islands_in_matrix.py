76a3e4d244a98b6babc58ec8b80fc5f6954092fa
Kirill <89972203+kirillklimenko@users.noreply.github.com>
2025-01-12 12:10:35 +0200

    Update islands_in_matrix.py

--- islands_in_matrix.py
+++ islands_in_matrix.py
@@ -1,51 +1,44 @@
 import numpy as np
 
 
-class Graph:
-    '''Count islands in boolean 2D matrix'''
-    def __init__(self, grid):
-        self.rows, self.cols = grid.shape
-        self.grid = grid.copy()
-        self.row_nbr = [-1, -1, -1, 0, 0, 1, 1, 1]
-        self.col_nbr = [-1, 0, 1, -1, 1, -1, 0, 1]
+class IslandMap:
+    def __init__(self, matrix: list[list[bool]]) -> None:
         self.islands = 0
+        self.grid = np.array(matrix, dtype=np.bool_)
+        self.rows, self.cols = self.grid.shape
+        self.visited = np.zeros(self.grid.shape, dtype=np.bool_)
+        self.directions = [
+            (-1, 1),  (0, 1),  (1, 1),
+            (-1, 0),           (1, 0),
+            (-1, -1), (0, -1), (1, -1)
+        ]
 
-
-    def isSafe(self, i, j, visited):
+    def is_land(self, i: int, j: int) -> bool:
         return (
-            i >= 0 and
-            i < self.rows and
-            j >= 0 and
-            j < self.cols and
-            not visited[i, j] and
-            self.grid[i, j]
+            0 <= i < self.rows and 0 <= j < self.cols
+            and not self.visited[i, j] and self.grid[i, j]
         )
-            
 
-    def DFS(self, i, j, visited):
-        visited[i, j] = True
-        for k in range(8):
-            ii, jj = i + self.row_nbr[k], j + self.col_nbr[k]
-            if self.isSafe(ii, jj, visited):
-                self.DFS(ii, jj, visited)
+    def dfs(self, i: int, j: int) -> None:
+        self.visited[i, j] = True
+        for dx, dy in self.directions:
+            x, y = i + dx, j + dy
+            if self.is_land(x, y):
+                self.dfs(x, y)
 
-
-    def countIslands(self):
-        visited = np.zeros((self.rows, self.cols), dtype=np.bool_)
-
+    def count_islands(self) -> int:
         for i in range(self.rows):
             for j in range(self.cols):
-                # If a cell with value 1 is not visited yet, then new island found
-                if self.grid[i, j] and not visited[i, j]:
+                # If a cell with value 1 is not visited yet, then island found
+                if self.grid[i, j] and not self.visited[i, j]:
                     # Visit all cells in this island and increment island count
-                    self.DFS(i, j, visited)
+                    self.dfs(i, j)
                     self.islands += 1
 
         return self.islands
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     grid = np.random.choice([0, 0, 1], size=(10, 10))
     print(grid)
-    print('Islands =', Graph(grid).countIslands())
+    print("Islands =", IslandMap(grid).count_islands())
